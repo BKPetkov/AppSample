4d2772cc93d69cf9bca7fc35630ed3494060d5c3
BKPetkov <petkovbrian@gmail.com>
2017-05-19 10:23:14 -0400

    Added more table support, sample contractor data. Todo: Search/Filtering

---
+++ AppSample/Contractor.swift
@@ -0,0 +1,20 @@
+//
+//  Contractor.swift
+//  AppSample
+//
+//  Created by Brian on 5/19/17.
+//  Copyright © 2017 Brian. All rights reserved.
+//
+
+import Foundation
+
+
+class Contractor {
+    var category : String
+    var name : String
+    
+    init(category: String, name: String) {
+        self.category = category
+        self.name = name
+    }
+}
