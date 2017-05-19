1f6277a30c7d9d9da615a006efb1344f96d644cb
BKPetkov <petkovbrian@gmail.com>
2017-05-19 08:59:10 -0400

    Added support for Image uploads from Camera/Gallery to Firebase Storage. ImagePicker library for photo selection.

--- AppSample/CameraViewController.swift
+++ AppSample/CameraViewController.swift
@@ -13,12 +13,15 @@ import FirebaseAnalytics
 import FirebaseDatabase
 import ImagePicker
 import Lightbox
+import FirebaseStorage
 
 class CameraViewController: UIViewController, UITextViewDelegate, ImagePickerDelegate {
 
     // MARK: Properties
     
     @IBOutlet weak var estaTextField: UITextView!
+    
+    @IBOutlet weak var testImgView: UIImageView!
     
     @IBOutlet weak var estaTextBtn: UIButton!
     
@@ -34,6 +37,7 @@ class CameraViewController: UIViewController, UITextViewDelegate, ImagePickerDel
     
         print("debug: CameraViewController is now active")
         
+
 
     }
     
@@ -62,25 +66,10 @@ class CameraViewController: UIViewController, UITextViewDelegate, ImagePickerDel
     }
     
 
-    
-    
-    
-
-    override func didReceiveMemoryWarning() {
+  override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     
     // MARK: Actions
@@ -99,19 +88,92 @@ class CameraViewController: UIViewController, UITextViewDelegate, ImagePickerDel
         
         print(estaTextField.text)
         
-       // let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        
-        
-        
-       // let controller = storyboard.instantiateViewController(withIdentifier: "PhotoPickerViewController")
-        
-        
-        let imagePickerController = ImagePickerController()
-        imagePickerController.delegate = self
-        present(imagePickerController, animated: true, completion: nil)
-        
         
     }
+    
+    
+    
+    @IBAction func pickPhoto(_ sender: Any) {
+        
+        let imagePickerController = ImagePickerController()
+        imagePickerController.imageLimit = 1 //could increase
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
+
+        }
+    
+    
+    @IBAction func sendPhoto(_ sender: UIButton) {
+        
+        // print("send photo btn pressed!")
+        
+        let string2 = "Test String Data"
+        let localFile = string2.data(using: .utf8)
+        
+        
+        // Uncomment line below to use actual image from gallery
+        // Upload may take some time depending on the connection & image size
+        
+        //   let localFile:  NSData = UIImagePNGRepresentation(testImgView.image!)! as NSData
+        
+        
+        // Create file metadata - not necessary
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        
+        let storage2 = Storage.storage(url: "gs://iossample-9fd88.appspot.com")
+        let photosRef = storage2.reference().child("images/myphoto4.jpg")
+        
+
+        let uploadTask = photosRef.putData(localFile as! Data, metadata: nil)
+        
+        // From AppleDev API: Listen for state changes, errors, and completion of the upload.
+        uploadTask.observe(.resume) { snapshot in
+            // Upload resumed, also fires when the upload starts
+        }
+        
+        uploadTask.observe(.pause) { snapshot in
+            // Upload paused
+        }
+        
+        uploadTask.observe(.progress) { snapshot in
+            // Upload reported progress
+            let percentComplete = 100.0 * Double(snapshot.progress!.completedUnitCount)
+                / Double(snapshot.progress!.totalUnitCount)
+        }
+        
+        uploadTask.observe(.success) { snapshot in
+            // Upload completed successfully
+        }
+        
+        uploadTask.observe(.failure) { snapshot in
+            if let error = snapshot.error as? NSError {
+                switch (StorageErrorCode(rawValue: error.code)!) {
+                case .objectNotFound:
+                    // File doesn't exist
+                    break
+                case .unauthorized:
+                    // User doesn't have permission to access file
+                    break
+                case .cancelled:
+                    // User canceled the upload
+                    break
+                    
+                    /* ... */
+                    
+                case .unknown:
+                    // Unknown error occurred, inspect the server response
+                    break
+                default:
+                    // A separate error occurred. This is a good place to retry the upload.
+                    break
+                }
+            }
+        }
+        
+    }
+    
     
       // MARK: - ImagePickerDelegate
     
@@ -132,7 +194,14 @@ class CameraViewController: UIViewController, UITextViewDelegate, ImagePickerDel
     
     func doneButtonDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
         imagePicker.dismiss(animated: true, completion: nil)
+        print (images.count) // shouldn't be more than the limit above
+        testImgView.image = nil
+        testImgView.image = images[0]
+        
+        
     }
+    
+    
     
     
     
